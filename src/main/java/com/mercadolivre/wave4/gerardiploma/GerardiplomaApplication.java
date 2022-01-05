1efee1612818022bcf3e883d5afb5c7549ef72be
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2022-01-05 10:03:50 -0300

    first commit

---
+++ src/main/java/com/mercadolivre/wave4/gerardiploma/GerardiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.wave4.gerardiploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerardiplomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerardiplomaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadolivre/wave4/gerardiploma/GerardiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.wave4.gerardiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerardiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
