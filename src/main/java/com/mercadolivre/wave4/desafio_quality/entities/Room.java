bc6abbef783204c9f1296f9522a36ba08bc8dbf2
Samuel <samuel.stalschus@mercadolivre.com>
2022-01-05 14:40:20 -0300

    Feat: Fix: fixing entities

--- src/main/java/com/mercadolivre/wave4/desafio_quality/entities/Room.java
+++ src/main/java/com/mercadolivre/wave4/desafio_quality/entities/Room.java
@@ -3,12 +3,14 @@ package com.mercadolivre.wave4.desafio_quality.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,53 +19,14 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private Double width;
+
+    @Column
     private Double length;
 
-    @JsonIgnore
-    @Transient
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "property_id")
-    private Property property;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getWidth() {
-        return width;
-    }
-
-    public void setWidth(Double width) {
-        this.width = width;
-    }
-
-    public Double getLength() {
-        return length;
-    }
-
-    public void setLength(Double length) {
-        this.length = length;
-    }
-
-    public Long getProperty() {
-        return property.getId();
-    }
-
-    public void setProperty(Property property) {
-        this.property = property;
-    }
 }
