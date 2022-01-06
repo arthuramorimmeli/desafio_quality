8960f93b1cfc0ceb93794358d81c26d053a5d2e2
Samuel <samuel.stalschus@mercadolivre.com>
2022-01-06 16:08:32 -0300

    Feat: Add - District create and list

--- src/main/java/com/mercadolivre/wave4/desafio_quality/entities/District.java
+++ src/main/java/com/mercadolivre/wave4/desafio_quality/entities/District.java
@@ -16,14 +16,9 @@ public class District {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotNull(message = "O bairro não pode estar vazio")
-    @NotEmpty(message = "O bairro não pode estar vazio")
-    @Size(max = 45, message = "O comprimento do bairro não pode exceder 45 caracteres.")
     @Column
     private String name;
 
-    @NotNull(message = "O valor do metro quadrado no bairro não pode estar vazio")
-    @DecimalMax(value = "13.0", message = "Valor maximo é 13")
     @Column
     private Double footageValue;
 }
